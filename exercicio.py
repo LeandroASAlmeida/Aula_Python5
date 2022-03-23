feace4848a6121e7828710136a42dddac6d8cfa5
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-03-22 21:59:53 -0300

    Delete exercicio.py

--- exercicio.py
+++
@@ -1,8 +0,0 @@
-# Elabore um programa para manipular informações em uma lista.
-# Os itens da lista deverão ser arquivos de informática
-# A cada operação executada (leitura, inserção, exclusão e edição da lista deverá gravar um log)
-# O arquivo será "log.txt"
-# Implementar a função grava log e demais funções em arquivo separado.
-# A função gravaLog deverá receber uma mensagem de parâmetro que será gravada.
-# Tempo estimado: 20 minutos
-
